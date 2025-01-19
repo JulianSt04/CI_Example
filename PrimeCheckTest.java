ef7398c428fedc75e3ff61e25b1943e595869dc8
Julian_St04 <staegegaming@gmail.com>
2025-01-19 14:17:42 +0100

    add PrimeCheckTest tests

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -24,13 +24,18 @@ public class PrimeCheckTest {
   @Test
   public void testPrimes() {
     /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primes = {2,3,5,7,11,13,17,19,23,29,31,37,1237,991};
+    for (int i=0; i<primes.length; i++){
+    assertEquals("True if is Prime", true, PrimeCheck.isPrime(primes[i]));
+    }
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] nonPrimes = {4,6,10,450,333,256,144,18,100000,49,63};
+    for (int i=0; i<primes.length; i++){
+    assertEquals("False if is no Prime", false, PrimeCheck.isPrime(nonPrimes[i]));
+    }
   }
 }
